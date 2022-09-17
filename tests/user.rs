bdc8304bf9072b34f27aa59f779cfc681db6e8e1
Oliver Forral <intrepion@gmail.com>
2022-09-17 15:31:28 -0600

    green

--- tests/user.rs
+++ tests/user.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 mod tests {
+    use actix_web::{test, web, App};
+    use intrepion_users_json_rpc_server_sqlite_rust_diesel_actix_web::routes::index;
+
     #[actix_web::test]
     async fn test_not_logged_in() {
-        let app = test::init_service(App::new().route("/", web::get().to(index))).await;
+        let _app = test::init_service(App::new().route("/", web::get().to(index))).await;
     }
 }
